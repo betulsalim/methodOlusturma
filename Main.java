c5ea29ff55564e9a46077d7fce2ccd1cf205221b
betulsalim <112887429+betulsalim@users.noreply.github.com>
2022-11-07 15:50:57 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int pozitif(int num,int y){
+
+        if(num>y){
+            return num;
+        }
+
+        else{
+            System.out.print(num+ " ");
+            return pozitif(num+5,y);
+        }
+
+    }
+
+    static int negatif(int num){
+        if(num<=0){
+            return num;
+
+        }
+        else{
+            System.out.print(num + " ");
+            return negatif(num-5);
+        }
+    }
+
+    public static void main(String[] args) {
+            int num;
+            Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Sayiyi giriniz : ");
+            num = input.nextInt();
+
+            pozitif(negatif(num),num);
+
+            if (num == 0){
+                break;
+            }
+        break;
+
+        }
+
+
+
+    }
+}
